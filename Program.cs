63ea9429f0cc543a5715fd95ff8e4c2b42cf9838
rafayoko <rafaelatakitani@outlook.com>
2021-10-08 12:26:11 -0300

    Adicionando comando para imprimir a data de hoje

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ProjetoVersionandoComGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(DataTime.Now.ToLongDateString());
         }
     }
 }

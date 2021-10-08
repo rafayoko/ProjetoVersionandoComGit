c6bf36c7550d62b60a2b085a035437c37e54c36e
rafayoko <rafaelatakitani@outlook.com>
2021-10-08 13:19:51 -0300

    Adicionada nova linha incluindo a versão da aplicação

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace ProjetoVersionandoComGit
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine(DataTime.Now.ToLongDateString());
+            Console.WriteLine("Vesão da aplicação: 1.0");
         }
     }
 }

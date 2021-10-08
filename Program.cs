629ab98762d98b88a3a792c5cb44355f01266050
rafayoko <rafaelatakitani@outlook.com>
2021-10-08 12:21:20 -0300

    Meu primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoVersionandoComGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
